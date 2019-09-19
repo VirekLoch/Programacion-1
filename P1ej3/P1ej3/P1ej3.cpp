9d00e95ed831ab42f6e78d4b1ecf039b44a1a4e5
Virek_Loch <Virek_Loch@LAPTOP-3CL2HM03>
2019-09-19 15:14:01 +0200

    C

---
+++ P1ej3/P1ej3/P1ej3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+char car;
+int v1, v2, res;
+
+int main()
+{
+	cout << "Estoy aprendiendo Progeramacion I:\nAhora introduce un caracter\n";
+	cin >> car;
+	char e3(car);
+	cout << "Tu caracter es : " << car ;
+	cout << "\nIntroduce dos numeros a multiplicar: \n ";
+	cin >> v1;
+	cout << "* \n";
+	cin >> v2;
+	res = v1 * v2;
+	cout << "\n" << res << "\n";
+}
+char e3()
+{
+	cin >> car;
+	return(car);
+}
+int calculos(v1, v2)
+{
+	sum = v1 + v2;
+	res = v1-v2:
+	mult = v1 * v2;
+	div v1 / v2;
+}
+	
